c44e532f5f9d5d0c9bbfcf0e7bb306184165ffc8
jatintiwari1 <jatin.tiwari1807@gmail.com>
2026-09-16 12:14:27 +0530

    Delete sql/sql/02_load_raw.sql

--- sql/sql/02_load_raw.sql
+++
@@ -1,9 +0,0 @@
-LOAD DATA LOCAL INFILE 'C:/data/faostat_crop_production_raw.csv'
-INTO TABLE faostat_crop_raw
-FIELDS TERMINATED BY ','
-OPTIONALLY ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(area_code, area_name, item_code, item_name,
- element_code, element_name, year_value, unit,
- value_raw, flag);
